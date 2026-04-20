1679ba90d3c45c2c4511dd79782795ac738837ec
sabinMas <202682616+sabinMas@users.noreply.github.com>
2026-04-19 18:01:25 -0700

    practice interview with class mate 2 questions and piglatin method

---
+++ src/practiceTestInterview.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class practiceTestInterview {
+   
+
+   public static int wordsLongerThanLessThan(ArrayList<String> words, int n, int m){
+    int counter = 0;
+    for(String word : words){
+        if(word.length()>n && word.length()<m){
+            counter++;
+        }
+    }
+    return counter;
+   }
+
+   public static String longestWordGiven(String[] words, char n){
+    String longest = "";
+    
+    for(String word : words){
+        char firstLetter = word.charAt(0);
+        if(firstLetter == n && word.length() > longest.length()){
+            longest = word;
+        }
+    }
+    return longest;
+   }
+
+   public static String pigLatin(String sentence){
+    StringBuilder sb = new StringBuilder("");
+    String[] words = sentence.split(" ");
+
+    for(String word:words){
+        if("aeiou".indexOf(word.charAt(0)) != -1){
+            sb.append(word);
+        } else{
+           String alteredWord = word.substring(1) + word.charAt(0)+"ay";
+            sb.append(alteredWord);
+        }
+
+    }
+    return sb.toString();
+   }
+
+}
+    
+
